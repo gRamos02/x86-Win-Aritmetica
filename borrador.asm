163a248272dde72c494476accfec0745c1a16d9f
Gerardo Ramos <gerardoere02@gmail.com>
2024-11-21 22:26:19 -0600

    Se agrego la validacion para la entrada de numeros, tambien se agregaron saltos adicionales de linea pa que se vea mas bello y correcciones ortograficas

--- borrador.asm
+++ borrador.asm
@@ -4,9 +4,9 @@
 
 .DATA
 equipo DB 'Equipo 7:', '$'
-nombre1 DB 09h, 'Jose Ramon Romero Zavala - 22130584', '$'
+nombre1 DB 09h, 'José Ramón Romero Zavala - 22130584', '$'
 nombre2 DB 09h, 'Gerardo Enrique Ramos Espinoza - 21130599', '$'
-proyecto DB 'Aritmetica', '$'
+proyecto DB 'Aplicación de instrucciones de aritmética', '$'
 msg_prompt1 DB 'Introduce el primer numero: ', '$'
 msg_prompt2 DB 'Introduce el segundo numero: ', '$'
 msg_operaciones1 DB 'Operaciones: ', '$'
@@ -14,11 +14,13 @@ msg_operaciones2 DB 'Sumar: +', '$'
 msg_operaciones3 DB 'Restar: -', '$'
 msg_operaciones4 DB 'Multiplicar: *', '$'
 msg_operaciones5 DB 'Dividir: /', '$'
-msg_prompt3 DB 'Introduce la operacion deseada: ', '$'
+msg_prompt3 DB 'Introduce la operación deseada: ', '$'
 resultado DB 'El resultado es: ', '$'
 of_msg DB 'Hay sobreflujo!', '$'
+err_msg DB 'Símbolo invalido!', '$'
+error_msg DB 'Entrada invalida, por favor ingrese un numero.', '$'
 buffer DB 100 DUP('$')        ; Espacio para almacenar la entrada.
-nueva_linea DB 0Dh, 0Ah, '$'  ; 0D = CR y 0A = Salto de línea
+nueva_linea DB 0Dh, 0Ah, 0Ah, '$'  ; 0D = CR y 0A = Salto de línea
 num1 DB 0
 num2 DB 0
 sum DB 0
@@ -68,34 +70,66 @@ pedir_numero1:
     mov ah, 9h
     int 21h
 
+    leer_numero1:
     mov ah, 01h ; Leer un solo caracter
     int 21h
-    sub al, '0' ; Convertir ascii en valor numerico
+    cmp al, '0'
+    jb entrada_invalida1
+    cmp al, '9'
+    ja entrada_invalida1
 
+    sub al, '0' ; Convertir ascii en valor numerico
     mov num1, al ; Mover valor num. a num1
-    
-    LEA dx, nueva_linea
+
+    mov dx, OFFSET nueva_linea
     mov ah, 9h
     int 21h
-
     jmp pedir_numero2
+
+entrada_invalida1:
+    mov dx, OFFSET nueva_linea
+    mov ah, 9h
+    int 21h
+    mov dx, OFFSET error_msg
+    mov ah, 9h
+    int 21h
+    mov dx, OFFSET nueva_linea
+    mov ah, 9h
+    int 21h
+    jmp pedir_numero1
 
 pedir_numero2:
     mov dx, OFFSET msg_prompt2
     mov ah, 9h
     int 21h
 
+    leer_numero2:
     mov ah, 01h ; Leer un solo caracter
     int 21h
-    sub al, '0' ; Convertir ascii en valor numerico
+    cmp al, '0'
+    jb entrada_invalida2
+    cmp al, '9'
+    ja entrada_invalida2
 
-    mov num2, al ; Mover valor num. a num1
+    sub al, '0' ; Convertir ascii en valor numerico
+    mov num2, al ; Mover valor num. a num2
 
     mov dx, OFFSET nueva_linea
     mov ah, 9h
     int 21h
-
     jmp imprimir_menu
+
+entrada_invalida2:
+    mov dx, OFFSET nueva_linea
+    mov ah, 9h
+    int 21h
+    mov dx, OFFSET error_msg
+    mov ah, 9h
+    int 21h
+    mov dx, OFFSET nueva_linea
+    mov ah, 9h
+    int 21h
+    jmp pedir_numero2
 
 imprimir_menu:
     ; Mostrar las opciones del menú
@@ -163,7 +197,10 @@ imprimir_menu:
     mov dx, OFFSET nueva_linea
     mov ah, 9h
     int 21h
-    mov dx, OFFSET of_msg
+    mov dx, OFFSET err_msg
+    mov ah, 9h
+    int 21h
+    mov dx, OFFSET nueva_linea
     mov ah, 9h
     int 21h
     jmp imprimir_menu ; Volver al menú
